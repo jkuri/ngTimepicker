730df433ba8f270aba79f7302dd4835e9ec070b6
Tim Graboski <tim.graboski@aggregateiq.com>
2016-01-14 11:44:12 -0800

    Minified code; added offsets to position the div right below the input object; etc.

--- src/js/ngTimepicker.js
+++ src/js/ngTimepicker.js
@@ -1,219 +1,234 @@
-angular.module('jkuri.timepicker', [])
-
-.directive('ngTimepicker', ['$document', function($document) {
-
-	var setScopeValues = function (scope, attrs) {
-		scope.initTime = attrs.initTime || '11:00';
-		scope.step = attrs.step || '15';
-		scope.showMeridian = attrs.showMeridian || false;
-		scope.meridian = attrs.meridian || 'AM';
-		scope.theme = attrs.theme || '';
-		scope.editable = attrs.editable || true;
-	};
-
-	return {
-		restrict: 'EA',
-		scope: true,
-		require: '?ngModel',
-		link: function (scope, element, attrs, ngModel) {
-			setScopeValues(scope, attrs);
-			scope.opened = false;
-
-			var initTime = function () {
-				var time = scope.initTime.split(':');
-				scope.hour = time[0];
-				scope.minutes = time[1];
+(function(){
+	angular
+		.module('jkuri.timepicker', [])
+		.directive('ngTimepicker', ['$document', function($document) {
+			var setScopeValues = function (scope, attrs) {
+				scope.initTime = attrs.initTime || '11:00';
+				scope.step = attrs.step || '15';
+				scope.showMeridian = attrs.showMeridian || false;
+				scope.meridian = attrs.meridian || 'AM';
+				scope.theme = attrs.theme || '';
+				scope.editable = attrs.editable || true;
 			};
 
-			var isValid = function(){
-				if (isNaN(scope.hour) || isNaN(scope.minutes)){
-					return false;
-				}
-
-				return true;
-			};
-
-			var setTime = function () {
-				var time;
-				if (!scope.showMeridian) {
-					time = scope.hour + ':' + scope.minutes;
-					scope.viewValue = time;
-					ngModel.$setViewValue(time);
-				} else {
-					time = scope.hour + ':' + scope.minutes;
-					scope.viewValue = time + ' ' + scope.meridian;
-					time = convertFromMeridianHour() + ':' + scope.minutes;
-					ngModel.$setViewValue(time);
-				}
-			};
-
-			var convertFromMeridianHour = function () {
-				var hour = parseInt(scope.hour, 10);
+			var getPosition = function(element){
+				var de = document.documentElement;
+				var box = element.getBoundingClientRect();
+				var input_box = element.firstChild;
 				
-				// if it's 12AM or 12PM -> convert to 12 or 00
-				if (hour === 12 && scope.meridian === 'PM') return 12;
-				if (hour === 12 && scope.meridian === 'AM') return '00';
+				var top = box.top + window.pageYOffset - de.clientTop + input_box.offsetHeight;
+				var left = box.left + window.pageXOffset - de.clientLeft;
 
-				if (scope.meridian === 'PM') {
-					return hour + 12;
-				} else if (hour === 0){
-					return 12;
-				} else if (hour < 10) {
-					return '0' + hour;
-				} else {
-					return hour;
-				}
+				return { top: top - 1, left: left };
 			};
 
-			var reinitTime = function () {
-				var time = scope.initTime.split(':');
-				scope.hour = time[0];
-				scope.minutes = time[1];
+			return {
+				restrict: 'EA',
+				scope: true,
+				require: '?ngModel',
+				link: function (scope, element, attrs, ngModel) {
+					setScopeValues(scope, attrs);
+					scope.opened = false;
+					var position = getPosition(element[0]);
+					scope.left = position.left+'px';
+					scope.top = position.top+'px';
 
-				time = scope.hour + ':' + scope.minutes;
-				scope.viewValue = time;
-				ngModel.$setViewValue(time);
-			};
+					var initTime = function () {
+						var time = scope.initTime.split(':');
+						scope.hour = time[0];
+						scope.minutes = time[1];
+					};
 
-			scope.showTimepicker = function () {
-				scope.opened = true;
-			};
+					var isValid = function(){
+						if (isNaN(scope.hour) || isNaN(scope.minutes)){
+							return false;
+						}
 
-			scope.incrementHour = function () {
-				if (!scope.showMeridian) {
-					if (parseInt(scope.hour, 10) < 23) {
-						scope.hour = parseInt(scope.hour, 10) + 1;
-					} else {
-						scope.hour = 0;
-					}
-				} else {
-					if (parseInt(scope.hour, 10) === 12 ){
-						scope.hour = 1;
-					} else if (parseInt(scope.hour, 10) === 11) {
-						scope.hour = 12;
-						scope.toggleMeridian();
-					} else {
-						scope.hour = parseInt(scope.hour, 10) + 1;
-					}
-				}
+						return true;
+					};
 
-				if (parseInt(scope.hour, 10) < 10) {
-					scope.hour = '0' + scope.hour;
-				}
+					var setTime = function () {
+						var time;
+						if (!scope.showMeridian) {
+							time = scope.hour + ':' + scope.minutes;
+							scope.viewValue = time;
+							ngModel.$setViewValue(time);
+						} else {
+							time = scope.hour + ':' + scope.minutes;
+							scope.viewValue = time + ' ' + scope.meridian;
+							time = convertFromMeridianHour() + ':' + scope.minutes;
+							ngModel.$setViewValue(time);
+						}
+					};
 
-				setTime();
-			};
+					var convertFromMeridianHour = function () {
+						var hour = parseInt(scope.hour, 10);
+						
+						// if it's 12AM or 12PM -> convert to 12 or 00
+						if (hour === 12 && scope.meridian === 'PM') return 12;
+						if (hour === 12 && scope.meridian === 'AM') return '00';
 
-			scope.decreaseHour = function () {
-				if (!scope.showMeridian) {
-					if (parseInt(scope.hour, 10) === 0) {
-						scope.hour = 23;
-					} else {
-						scope.hour = parseInt(scope.hour, 10) - 1;
-					}
-				} else {
-					if (parseInt(scope.hour, 10) === 1) {
-						scope.hour = 12;
-					} else if (parseInt(scope.hour, 10) === 12) {
-						scope.hour = 11;
-						scope.toggleMeridian();
-					} else {
-						scope.hour = parseInt(scope.hour, 10) - 1;
-					}
-				}
+						if (scope.meridian === 'PM') {
+							return hour + 12;
+						} else if (hour === 0){
+							return 12;
+						} else if (hour < 10) {
+							return '0' + hour;
+						} else {
+							return hour;
+						}
+					};
 
-				if (parseInt(scope.hour, 10) < 10) {
-					scope.hour = '0' + scope.hour;
-				}
+					var reinitTime = function () {
+						var time = scope.initTime.split(':');
+						scope.hour = time[0];
+						scope.minutes = time[1];
 
-				setTime();
-			};
+						time = scope.hour + ':' + scope.minutes;
+						scope.viewValue = time;
+						ngModel.$setViewValue(time);
+					};
 
-			scope.incrementMinutes = function () {
-				scope.minutes = parseInt(scope.minutes, 10) + parseInt(scope.step, 10);
-				if (scope.minutes > 59) {
-					scope.minutes = '00';
-					scope.incrementHour();
-				}
-				setTime();
-			};
+					scope.showTimepicker = function () {
+						scope.opened = true;
+					};
 
-			scope.decreaseMinutes = function () {
-				scope.minutes = parseInt(scope.minutes, 10) - parseInt(scope.step, 10);
-				if (parseInt(scope.minutes, 10) < 0) {
-					scope.minutes = 60 - parseInt(scope.step, 10);
-					scope.decreaseHour();
-				}
-				if (parseInt(scope.minutes, 10) === 0) {
-					scope.minutes = '00';
-				}
-				setTime();
-			};
+					scope.incrementHour = function () {
+						if (!scope.showMeridian) {
+							if (parseInt(scope.hour, 10) < 23) {
+								scope.hour = parseInt(scope.hour, 10) + 1;
+							} else {
+								scope.hour = 0;
+							}
+						} else {
+							if (parseInt(scope.hour, 10) === 12 ){
+								scope.hour = 1;
+							} else if (parseInt(scope.hour, 10) === 11) {
+								scope.hour = 12;
+								scope.toggleMeridian();
+							} else {
+								scope.hour = parseInt(scope.hour, 10) + 1;
+							}
+						}
 
-			scope.keyDown = function (evt, which) {
-				if(evt.keyCode === 40) { // decrement
-					if (which == 'minutes'){
-						scope.decreaseMinutes();
-					} else {
-						scope.decreaseHour();
-					}
-				} else if (evt.keyCode === 38) { // increment
-					if (which == 'minutes'){
-						scope.incrementMinutes();
-					} else {
-						scope.incrementHour();
-					}
-				}
-			};
+						if (parseInt(scope.hour, 10) < 10) {
+							scope.hour = '0' + scope.hour;
+						}
 
-			scope.toggleMeridian = function () {
-				scope.meridian = (scope.meridian === 'AM') ? 'PM' : 'AM';
-				setTime();
-			};
+						setTime();
+					};
 
-			scope.edit = function () {
-				if (scope.editable && isValid()){
-					setTime();
-				}
-			};
+					scope.decreaseHour = function () {
+						if (!scope.showMeridian) {
+							if (parseInt(scope.hour, 10) === 0) {
+								scope.hour = 23;
+							} else {
+								scope.hour = parseInt(scope.hour, 10) - 1;
+							}
+						} else {
+							if (parseInt(scope.hour, 10) === 1) {
+								scope.hour = 12;
+							} else if (parseInt(scope.hour, 10) === 12) {
+								scope.hour = 11;
+								scope.toggleMeridian();
+							} else {
+								scope.hour = parseInt(scope.hour, 10) - 1;
+							}
+						}
 
-			$document.on('click', function (e) {
-				if (element !== e.target && !element[0].contains(e.target)) {
-					scope.$apply(function () {
-						scope.opened = false;
+						if (parseInt(scope.hour, 10) < 10) {
+							scope.hour = '0' + scope.hour;
+						}
+
+						setTime();
+					};
+
+					scope.incrementMinutes = function () {
+						scope.minutes = parseInt(scope.minutes, 10) + parseInt(scope.step, 10);
+						if (scope.minutes > 59) {
+							scope.minutes = '00';
+							scope.incrementHour();
+						}
+						setTime();
+					};
+
+					scope.decreaseMinutes = function () {
+						scope.minutes = parseInt(scope.minutes, 10) - parseInt(scope.step, 10);
+						if (parseInt(scope.minutes, 10) < 0) {
+							scope.minutes = 60 - parseInt(scope.step, 10);
+							scope.decreaseHour();
+						}
+						if (parseInt(scope.minutes, 10) === 0) {
+							scope.minutes = '00';
+						}
+						setTime();
+					};
+
+					scope.keyDown = function (evt, which) {
+						if(evt.keyCode === 40) { // decrement
+							if (which == 'minutes'){
+								scope.decreaseMinutes();
+							} else {
+								scope.decreaseHour();
+							}
+						} else if (evt.keyCode === 38) { // increment
+							if (which == 'minutes'){
+								scope.incrementMinutes();
+							} else {
+								scope.incrementHour();
+							}
+						}
+					};
+
+					scope.toggleMeridian = function () {
+						scope.meridian = (scope.meridian === 'AM') ? 'PM' : 'AM';
+						setTime();
+					};
+
+					scope.edit = function () {
+						if (scope.editable && isValid()){
+							setTime();
+						}
+					};
+
+					$document.on('click', function (e) {
+						if (element !== e.target && !element[0].contains(e.target)) {
+							scope.$apply(function () {
+								scope.opened = false;
+							});
+						}
 					});
-				}
-			});
 
-			initTime();
-			setTime();
+					initTime();
+					setTime();
 
-		},
-		template:
-			'<input type="text" ng-focus="showTimepicker()" ng-value="viewValue" class="ng-timepicker-input" ng-readonly="true">' +
-			'<div class="ng-timepicker" ng-show="opened" ng-class="{\'red\': theme === \'red\', \'green\': theme === \'green\', \'blue\': theme === \'blue\'}">' +
-			'  <table>' +
-			'    <tbody>' +
-			'    <tr>' +
-			'        <td class="act noselect" ng-click="incrementHour()"><i class="fa fa-angle-up"></i></td>' +
-			'        <td></td>' +
-			'        <td class="act noselect" ng-click="incrementMinutes()"><i class="fa fa-angle-up"></i></td>' +
-			'        <td class="act noselect" ng-click="toggleMeridian()" ng-show="showMeridian"><i class="fa fa-angle-up"></i></td>' +
-			'      </tr>' +
-			'      <tr>' +
-			'        <td><input type="text" ng-model="hour" ng-readonly="!editable" ng-keydown="keyDown($event, \'hours\');" ng-change="edit()"></td>' +
-			'        <td>:</td>' +
-			'        <td><input type="text" ng-model="minutes" ng-readonly="!editable" ng-keydown="keyDown($event, \'minutes\');" ng-change="edit()"></td>' +
-			'        <td ng-show="showMeridian"><input type="text" ng-model="meridian" ng-readonly="true"></td>' +
-			'      </tr>' +
-			'      <tr>' +
-			'        <td class="act noselect" ng-click="decreaseHour()"><i class="fa fa-angle-down"></i></td>' +
-			'        <td></td>' +
-			'        <td class="act noselect" ng-click="decreaseMinutes()"><i class="fa fa-angle-down"></i></td>' +
-			'        <td class="act noselect" ng-click="toggleMeridian()" ng-show="showMeridian"><i class="fa fa-angle-down"></i></td>' +
-			'      </tr>' +
-			'  </table>' +
-			'</div>'
-	};
+				},
+				template:
+					'<input type="text" ng-focus="showTimepicker()" ng-value="viewValue" class="ng-timepicker-input" ng-readonly="true">' +
+					'<div class="ng-timepicker" ng-show="opened" ng-style="{\'left\': left, \'top\': top}">' +
+					'  <table>' +
+					'    <tbody>' +
+					'    <tr>' +
+					'        <td class="act noselect" ng-click="incrementHour()"><i class="fa fa-angle-up"></i></td>' +
+					'        <td></td>' +
+					'        <td class="act noselect" ng-click="incrementMinutes()"><i class="fa fa-angle-up"></i></td>' +
+					'        <td class="act noselect" ng-click="toggleMeridian()" ng-show="showMeridian"><i class="fa fa-angle-up"></i></td>' +
+					'      </tr>' +
+					'      <tr>' +
+					'        <td><input type="text" ng-model="hour" ng-readonly="!editable" ng-keydown="keyDown($event, \'hours\');" ng-change="edit()"></td>' +
+					'        <td>:</td>' +
+					'        <td><input type="text" ng-model="minutes" ng-readonly="!editable" ng-keydown="keyDown($event, \'minutes\');" ng-change="edit()"></td>' +
+					'        <td ng-show="showMeridian"><input type="text" ng-model="meridian" ng-readonly="true"></td>' +
+					'      </tr>' +
+					'      <tr>' +
+					'        <td class="act noselect" ng-click="decreaseHour()"><i class="fa fa-angle-down"></i></td>' +
+					'        <td></td>' +
+					'        <td class="act noselect" ng-click="decreaseMinutes()"><i class="fa fa-angle-down"></i></td>' +
+					'        <td class="act noselect" ng-click="toggleMeridian()" ng-show="showMeridian"><i class="fa fa-angle-down"></i></td>' +
+					'      </tr>' +
+					'  </table>' +
+					'</div>'
+			};
 
-}]);
+		}]);
+}());
